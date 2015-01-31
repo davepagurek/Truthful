edfd97ad60eebaff68dd9c17a968516b92cd6246
Dave Pagurek <dave@pahgawks.com>
2015-01-30 21:56:27 -0500

    Move table-creating code into Truthful.js

--- js/index.js
+++ js/index.js
@@ -38,96 +38,19 @@ var calculate = function(input) {
 
         document.getElementById("result").innerHTML = "";
 
-        var inputFunction = [];
-        for (var i=0; i<input.length; i++) {
-            inputFunction.push(Truthful.createExpression(input[i]));
-        }
+        var table = Truthful.truthTable(input);
 
         //If there are no errors, show the table
         if (!Truthful.hasErrors()) {
-            var inputFormula = document.createElement("div");
-            var variablesObj = {};
-            for (i=0; i<inputFunction.length; i++) {
-                var fnVariables = inputFunction[i].variables();
-                for (var key in fnVariables) {
-                    variablesObj[key] = 1;
-                }
-            }
-            var variables = [];
-            for (var key in variablesObj) {
-                variables.push(key);
-            }
-            variables.sort();
-
-            var htmlTable = "";
-
-            var table = [];
-            var col=0;
-            var totalRows = Math.pow(2, variables.length);
-            for (var key in variables) {
-                table.push([]);
-                var value = 0;
-                for (var row=0; row<totalRows; row++) {
-                    table[col].push(value);
-                    if ((row+1)%Math.pow(2, variables.length-col-1) == 0) {
-                        if (value) {
-                            value = 0;
-                        } else {
-                            value = 1;
-                        }
-                    }
-                }
-                col++;
-            }
-
-            for (var i=0; i<inputFunction.length; i++) {
-                table.push([]);
-            }
-            for (var row=0; row<totalRows; row++) {
-                var values = {};
-                for (var column=0; column<table.length-1; column++) {
-                    values[variables[column]]=table[column][row];
-                }
-
-                for (i=0; i<inputFunction.length; i++) {
-                    table[variables.length+i].push(inputFunction[i].result(values));
-                }
-            }
-
-            htmlTable += "<table><tr>";
-
-            for (var i=0; i<variables.length; i++) {
-                htmlTable += "<th>" + variables[i] + "</th>";
-            }
-            for (i=0; i<input.length; i++) {
-                htmlTable += "<th>" + input[i] + "</th>";
-            }
-            htmlTable += "</tr>";
-
-            for (var row=0; row<totalRows; row++) {
-                htmlTable += "<tr>";
-                for (var column=0; column<table.length; column++) {
-                    htmlTable += "<td class='" + (table[column][row]?"on":"off") + ((column==variables.length)?" firstResult":"") + "'>" + table[column][row] + "</td>";
-                }
-                htmlTable += "</tr>";
-            }
-
-            htmlTable += "</table>";
-
-            //console.table(table);
-            inputFormula.innerHTML = htmlTable;
-
-
-
-            document.getElementById("result").appendChild(inputFormula);
-            show(document.getElementById("wrapper"));
+            document.getElementById("result").appendChild(table);
 
             //Otherwise, show the errors, then clear the error list
         } else {
             document.getElementById("result").appendChild(Truthful.displayErrors());
-            show(document.getElementById("wrapper"));
+
             Truthful.clearErrors();
         }
+        show(document.getElementById("wrapper"));
     }, 800);
 
 };
